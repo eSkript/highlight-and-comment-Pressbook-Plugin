45aec58b5ae6209a4069c198dee92db806d461a2
unknown <mlorin@ethz.ch>
2017-10-05 20:21:16 +0200

    position for settings updated

--- public/js/highlight-comment-public-menu.js
+++ public/js/highlight-comment-public-menu.js
@@ -5,19 +5,26 @@ $(function () {
 	});
 	
 	function highlighterMenu() {
-		$(".third-block-wrap").append('<div class="third-block clearfix"><h2>Highlighter</h2><p><label><input type="checkbox" id="highlight_activate" name="activate" value="activate" checked>use highlighter</label></p><p><button id="highlight_summary">show all highlights</button></p></div><p></p>');
+        if($("#user_settings").length>0){
+           $("#user_settings").append('<div><p><label><input type="checkbox" id="highlight_activate" name="activate" value="activate" checked>use highlighter</label></p></div>');
+        }else{ //for downwards compadibility
+            $(".third-block-wrap").append('<div class="third-block clearfix"><h2>Highlighter</h2><p><label><input type="checkbox" id="highlight_activate" name="activate" value="activate" checked>use highlighter</label></p><p><button id="highlight_summary">show all highlights</button></p></div><p></p>');
+        }
+
 
 		if (highlight_vars.settings.indexOf("false") != -1) {
 			$("#highlight_activate").prop('checked', false);
-			$("#highlight_summary").hide();
+			//$("#highlight_summary").hide();
 		}
 
 		$("#highlight_activate").change(function () {
-			if (this.checked) {
+			/*
+            if (this.checked) {
 				$("#highlight_summary").show();
 			} else {
 				$("#highlight_summary").hide();
 			}
+            */
 
 			var data = {
 				'action': 'highlight_save_settings',
